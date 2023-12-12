64ad2c752593edf404a91d1ad8ad2f2828c049d7
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2023-12-12 09:22:02 +0300

    Absolute & Sliding Expiration

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Xml.Linq;
+
+namespace InMemory.Caching.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        ////Cache mekanizmasý
+        //[HttpGet("set/{name}")]
+        //public void SetName(string name)
+        //{
+        //    _memoryCache.Set("name", name);
+        //}
+
+        ////Cache'i çekme mekanizmasý
+        //[HttpGet]
+        //public string GetName()
+        //{
+        //    if (_memoryCache.TryGetValue<string>("name", out string name))
+        //    {
+        //        return name.Substring(3);
+        //    }
+        //    return "";
+        //    //var name = _memoryCache.Get<string>("name");
+        //    //return name.Substring(3);
+        //}
+
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
